3fd26ae915f98ef158fa9123fb1540240fd5fb4d
Yousuf <yousufidrees123@gmail.com>
2025-09-13 14:22:20 +0300

    fix: Update module exports in configuration files for compatibility

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
